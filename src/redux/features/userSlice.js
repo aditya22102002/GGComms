c43a430d2a4e52a77fe612fa856fa6e4ff77bba1
aditya22102002 <khemka.adityaraj@gmail.com>
2025-04-18 10:29:34 +0530

    getUsers

--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -6,7 +6,7 @@ const API_URL = 'https://ggcomms-backend.onrender.com';
 
 export const getCurrentUser = createAsyncThunk('user/get', async (_, thunkAPI) => {
   try {
-    const response = await axios.get(`${API_URL}/api/v1/users/getUser`, {}, { withCredentials: true });
+    const response = await axios.get(`${API_URL}/api/v1/users/getUser`, { withCredentials: true });
     return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.response.data.message || 'Failed to get user');
